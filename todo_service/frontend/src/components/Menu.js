414696dd4be9de1e0a78ea9460a987e42842c119
nat0t <nataniel1987@gmail.com>
2021-05-17 14:49:30 +0300

    Added components 'Menu', 'Footer', 'User' to frontend.

---
+++ todo_service/frontend/src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Menu = () => {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <span className="navbar-brand mb-0 h1">Menu</span>
+        </nav>
+    )
+}
+
+
+export default Menu
