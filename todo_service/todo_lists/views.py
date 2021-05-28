c77763a8e60902b51f350cdd42bf4147b280252d
nat0t <nataniel1987@gmail.com>
2021-05-28 14:52:48 +0300

    Changed API for projects.

--- todo_service/todo_lists/views.py
+++ todo_service/todo_lists/views.py
@@ -1,11 +1,20 @@
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
+
+from .filters import ProjectFilter
 from .models import Project, TODO
 from .serializers import ProjectModelSerializer, TODOModelSerializer
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
 class TODOModelViewSet(ModelViewSet):
--- todo_service/todo_service/settings.py
+++ todo_service/todo_service/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'corsheaders',
     'users',
     'todo_lists',
@@ -146,4 +147,5 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
