6aafba221e6417214bc5319b03317df579c715ae
nat0t <nataniel1987@gmail.com>
2021-06-01 12:51:08 +0300

    Added routing to frontend.

--- todo_service/frontend/src/components/Menu.js
+++ todo_service/frontend/src/components/Menu.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
 
 
 const Menu = () => {
     return (
-        <nav className="navbar navbar-dark bg-dark">
-            <span className="navbar-brand mb-0 h1">Menu</span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <span className="navbar-brand mb-0 h1">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <NavLink to='/' className="nav-link">Main</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to='/users' className="nav-link">Users</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to='/projects' className="nav-link">Projects</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to='/todo' className="nav-link">TODO</NavLink>
+                    </li>
+                    </ul>
+            </span>
         </nav>
     )
 }
