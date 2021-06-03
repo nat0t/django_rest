d2078f79f3eef54e59a7bd57f246405b468aecfa
nat0t <nataniel1987@gmail.com>
2021-06-03 22:49:42 +0300

    Added JWT authentication.

--- todo_service/todo_service/settings.py
+++ todo_service/todo_service/settings.py
@@ -159,5 +159,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
--- todo_service/todo_service/urls.py
+++ todo_service/todo_service/urls.py
@@ -17,6 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from users.views import UserCustomViewSet
 from todo_lists.views import ProjectModelViewSet, TODOModelViewSet
@@ -30,4 +35,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
