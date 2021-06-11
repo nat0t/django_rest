d8487bbc759858e6c8cb8d9af43b3acbaa8854ce
nat0t <nataniel1987@gmail.com>
2021-06-11 19:42:33 +0300

    Added tests.

--- todo_service/todo_lists/tests.py
+++ todo_service/todo_lists/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import (APIRequestFactory, force_authenticate,
+                                 APITestCase)
+from mixer.backend.django import mixer
+from users.models import User
+from .views import ProjectModelViewSet
+from .models import TODO
 
-# Create your tests here.
+
+class TestProjectViewSet(TestCase):
+    def test_get_list_unauthorized(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/projects/')
+        view = ProjectModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_list_authorized(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/projects/')
+        admin = User.objects.create_superuser('admin', 'admin@example.local',
+                                              'Q1234567q')
+        force_authenticate(request, admin)
+        view = ProjectModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TestTodoViewSet(APITestCase):
+    def test_get_list(self):
+        response = self.client.get('/api/todo_lists/')
+        self.client.login(username='django', password='A1234567a')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_edit_todo(self):
+        User.objects.create_superuser(username='testuser',
+                                      email='testuser@example.local',
+                                      password='Q1234567q')
+        todo = mixer.blend(TODO)
+        self.client.login(username='testuser', password='Q1234567q')
+        response = self.client.put(f'/api/todo_lists/{todo.id}/',
+                                   {
+                                       'text': 'Test text',
+                                       'project': todo.project,
+                                       'created_by': todo.created_by
+                                   })
+        print(response)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- todo_service/users/views.py
+++ todo_service/users/views.py
@@ -1,4 +1,3 @@
-# from rest_framework.viewsets import ModelViewSet
 from .models import User
 from .serializers import UserModelSerializer
 from rest_framework import mixins
@@ -6,9 +5,6 @@ from rest_framework import viewsets
 from rest_framework import renderers
 
 
-# class UserModelViewSet(ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserModelSerializer
 class UserCustomViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
                         mixins.UpdateModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.all()
