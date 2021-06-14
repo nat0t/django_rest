9078dbd0418d5ab3fc4abf289a77d0a6960e1748
nat0t <nataniel1987@gmail.com>
2021-06-14 19:18:31 +0300

    Added API documentation by swagger.

--- todo_service/todo_service/settings.py
+++ todo_service/todo_service/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django_filters',
     'corsheaders',
     'rest_framework.authtoken',
+    'drf_yasg',
     'users',
     'todo_lists',
 ]
--- todo_service/todo_service/urls.py
+++ todo_service/todo_service/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
@@ -25,6 +28,18 @@ from rest_framework_simplejwt.views import (
 
 from users.views import UserCustomViewSet
 from todo_lists.views import ProjectModelViewSet, TODOModelViewSet
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Library",
+      default_version='0.1',
+      description="Documentation to out project",
+      contact=openapi.Contact(email="admin@admin.local"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 router = DefaultRouter()
 router.register('users', UserCustomViewSet)
@@ -38,4 +53,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
